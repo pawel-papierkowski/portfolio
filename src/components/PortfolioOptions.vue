5ace41bb61161990e766525dab33ee136f690a70
Paweł Papierkowski <pawel.papierkowski@gmail.com>
2026-03-03 18:55:43 +0100

    Additional adjustments.

---
+++ src/components/PortfolioOptions.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import ThemeSwitcher from '@/components/ThemeSwitcher.vue';
+import LanguageSwitcher from '@/components/LanguageSwitcher.vue';
+</script>
+
+<template>
+    <aside class="menu">
+      <ThemeSwitcher />
+      <LanguageSwitcher />
+    </aside>
+</template>
+
+<style scoped></style>
